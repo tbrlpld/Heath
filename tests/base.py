48e0511668636406ad926594f8eb9dc009a1c19d
Tibor Leupold <tibor@lpld.io>
2019-12-19 14:33:59 -1000

    Move base test to separate file

---
+++ tests/base.py
@@ -0,0 +1,40 @@
+import unittest
+
+from pyramid import testing
+
+import transaction
+
+
+def dummy_request(dbsession):
+    return testing.DummyRequest(dbsession=dbsession)
+
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp(settings={
+            'sqlalchemy.url': 'sqlite:///:memory:'
+        })
+        self.config.include('heath.models')
+        settings = self.config.get_settings()
+
+        from heath.models import (
+            get_engine,
+            get_session_factory,
+            get_tm_session,
+            )
+
+        self.engine = get_engine(settings)
+        session_factory = get_session_factory(self.engine)
+
+        self.session = get_tm_session(session_factory, transaction.manager)
+
+    def init_database(self):
+        from heath.models.meta import Base
+        Base.metadata.create_all(self.engine)
+
+    def tearDown(self):
+        from heath.models.meta import Base
+
+        testing.tearDown()
+        transaction.abort()
+        Base.metadata.drop_all(self.engine)
