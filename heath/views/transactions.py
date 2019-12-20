231750a291c9baa4898273f6b7eb578e2bca45cf
Tibor Leupold <tibor@lpld.io>
2019-12-19 17:14:39 -1000

    Add form to create view

--- heath/views/transactions.py
+++ heath/views/transactions.py
@@ -2,6 +2,8 @@
 
 """Define views regarding transactions."""
 
+from typing import Dict
+
 from pyramid.request import Request
 from pyramid.response import Response
 from pyramid.view import view_config
@@ -11,5 +13,5 @@ from pyramid.view import view_config
     route_name="transaction_create",
     renderer="heath:templates/transactions/create.jinja2",
 )
-def create(request: Request) -> Response:
+def create(request: Request) -> Dict:
     return {}
--- tests/test_views/test_transactions.py
+++ tests/test_views/test_transactions.py
@@ -17,3 +17,8 @@ class TestFunctionalCreateTransactionView(FunctionalBaseTest):
     def test_create_view(self):
         resp = self.testapp.get("/create")
         self.assertEqual(resp.status_code, 200)
+        self.assertIn(b"Create Transaction", resp.body)
+        self.assertIn(b"<form", resp.body)
+        self.assertIn(b'name="description"', resp.body)
+        self.assertIn(b'name="amount"', resp.body)
+        self.assertIn(b'step="0.01"', resp.body)
