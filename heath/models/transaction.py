482be254ce5cca46757691123b3c77a415cd8787
Tibor Leupold <tibor@lpld.io>
2019-12-29 17:32:16 -1000

    Add amount float column

--- heath/models/transaction.py
+++ heath/models/transaction.py
@@ -8,6 +8,7 @@ from sqlalchemy import (
     Column,
     Integer,
     Numeric,
+    Float,
     Text,
     DateTime,
 )
@@ -20,5 +21,9 @@ class Transaction(Base):
     id = Column(Integer, primary_key=True, index=True)
     description = Column(Text, nullable=False)
     amount = Column(Numeric(precision=2), nullable=False)
+    amount_flaot = Column(
+        Float(precision=2, decimal_return_scale=2),
+        nullable=True,
+    )
 
     created = Column(DateTime, nullable=True, default=datetime.now)
