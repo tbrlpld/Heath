9f5f31734745fb5685a907cf15d483039753afd1
Tibor Leupold <tibor@lpld.io>
2019-12-29 17:44:22 -1000

    Amount float column
    Alembic can for some reason not deal well with dropping SQLite columns.
    I wanted to change the type of the amount column from numeric to float,
    because numeric leads to sqlalchemy warning about rounding etc.
    
    To get the desired change, I deleted all migrations and starting fresh
    with the correct configuration. Otherwise this would just be a pain in
    my ass.

--- heath/models/transaction.py
+++ heath/models/transaction.py
@@ -20,10 +20,9 @@ class Transaction(Base):
     __tablename__ = 'transactions'
     id = Column(Integer, primary_key=True, index=True)
     description = Column(Text, nullable=False)
-    amount = Column(Numeric(precision=2), nullable=False)
-    amount_flaot = Column(
+    amount = Column(
         Float(precision=2, decimal_return_scale=2),
-        nullable=True,
+        nullable=False,
     )
 
     created = Column(DateTime, nullable=True, default=datetime.now)
