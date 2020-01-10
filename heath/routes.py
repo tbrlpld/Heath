1db30c4662a8b40b5be52605c395dbface424488
Tibor Leupold <tibor@lpld.io>
2020-01-09 14:12:04 -1000

    Define transaction routes in separate function
    This allows to set the route prefix for all routes at once.

--- heath/routes.py
+++ heath/routes.py
@@ -1,10 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""Define routes of the application."""
+
+
+def transaction_routes(config):
+    """Define only transaction related routes."""
+    config.add_route('transaction.create', '/create')
+    config.add_route('transaction.list', '/')
+    config.add_route('transaction.detail', '/{transaction_id}')
+    config.add_route('transaction.update', '/{transaction_id}/update')
+    config.add_route('transaction.delete', '/{transaction_id}/delete')
+
+
 def includeme(config):
+    """Pull route configuration together."""
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
 
-    # Transactions
-    config.add_route('transaction.create', '/transaction/create')
-    config.add_route('transaction.list', '/transaction/')
-    config.add_route('transaction.detail', '/transaction/{transaction_id}')
-    config.add_route('transaction.update', '/transaction/{transaction_id}/update')
-    config.add_route('transaction.delete', '/transaction/{transaction_id}/delete')
+    config.include(transaction_routes, route_prefix="/transaction/")
